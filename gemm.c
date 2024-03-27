9e61d115e8dfd0a5a8675ebc9c7e4111b8be3480
Daniel-Sinkin <daniel.sinkin@gmx.de>
2024-03-27 05:10:21 +0100

    Implemented the blockwise matrix multiplication in C

--- gemm.c
+++ gemm.c
@@ -4,42 +4,77 @@
 #include <stdio.h>
 #include <assert.h>
 
-static const int N = 1024;
+static const int N = 4;
 
-#define BLOCK 32
+#define BLOCK_SIZE 2
 
 float A[N * N];
 float B[N * N];
 float C[N * N];
-float vla
+
+float A[N * N] = {
+    1, 2, 3, 4,
+    5, 6, 7, 8,
+    9, 10, 11, 12,
+    13, 14, 15, 16
+};
+
+float B[N * N] = {
+    1, -2, 3, -4,
+    5, -6, 7, -8,
+    -9, 10, -11, 12,
+    -13, -14, 15, -16
+};
+
+void matrixPrint(float *matrix, size_t num_rows, size_t num_cols) {
+    for (size_t y = 0; y < num_rows; y++) {
+        printf("[");
+        for (size_t x = 0; x < num_cols; x++) {
+            printf("%f", matrix[y * num_cols + x]);
+            if (x < num_cols - 1) {
+                printf(", ");
+            }
+        }
+        printf("]\n");
+    }
+}
 
 uint64_t nanos() {
     struct timespec start;
     clock_gettime(CLOCK_MONOTONIC_RAW, &start);
     return (uint64_t)start.tv_sec * 1e9 + (uint64_t)start.tv_nsec;
-
 }
 
 int main() {
-    assert(N % BLOCK == 0);
+    assert(N % BLOCK_SIZE == 0);
 
-    for (int k = 0; k < N * N; k++) A[k] = rand();
-    for (int k = 0; k < N * N; k++) A[k] = rand();
-
+    for(int i = 0; i < N*N; i++) C[i] = 0.0;
     uint64_t start = nanos();
-    for (int by = 0; by < N / BLOCK; by += BLOCK) {
-        for (int bx = 0; bx < N / BLOCK; bx += BLOCK) {
-            for(int y = by; y < by + BLOCK; y++) {
-                for(int x = bx; x < bx + BLOCK; x++) {
-                    int sum = 0;
-                    for(int k = 0; k < N; k++) {
-                        sum += A[y][k] * B[x][k];
+
+    for(int y = 0; y < N / BLOCK_SIZE; y++) {
+        for(int x = 0; x < N / BLOCK_SIZE; x++) {
+            for(int r = 0; r < N / BLOCK_SIZE; r++) {
+                for(int y_loc = 0; y_loc < BLOCK_SIZE; y_loc++) {
+                    for(int x_loc = 0; x_loc < BLOCK_SIZE; x_loc++) {
+                        float temp = 0.0;
+                        for(int i = 0; i < BLOCK_SIZE; i++) {
+                            float a_val = A[(y * BLOCK_SIZE + y_loc) * N + r * BLOCK_SIZE + i];
+                            float b_val = B[(r * BLOCK_SIZE + i) * N + x * BLOCK_SIZE + x_loc];
+                            temp += a_val * b_val;
+                        }
+                        C[((y * BLOCK_SIZE + y_loc)*N + x * BLOCK_SIZE + x_loc)] += temp;
                     }
-                    C[y][x] += sum;
                 }
             }
         }
     }
+
+    printf("A:\n");
+    matrixPrint(A, N, N);
+    printf("B:\n");
+    matrixPrint(B, N, N);
+    printf("C:\n");
+    matrixPrint(C, N, N);
     
     uint64_t end = nanos();
     double gfloppies = (2.0 * N * N * N) * 1e-9;
